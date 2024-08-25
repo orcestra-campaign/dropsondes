e701ac5cf4ab6bc201e7f7ec8fe26e26d4b8d784
Nina Robbins Blanch <nina.robbins@mpimet.mpg.de>
2024-08-25 14:32:24 -0100

    use data from ORCESTRA server for now

--- plotting/in_flight_quicklooks.py
+++ plotting/in_flight_quicklooks.py
@@ -2,14 +2,13 @@ from quickgrid import grid_together, derived_products
 from plotting import all_quicklook_plots
 from datetime import datetime
 
-flight_time = datetime(2024, 8, 22, 12, 0, 0)
+flight_time = datetime(2024, 8, 11, 0, 0, 0)
 flight_id = flight_time.strftime("%Y%m%d")
 
 # Flight directory
-dropsonde_dir = f"/Users/ninarobbins/Desktop/PhD/ORCESTRA/HALO-DATA/{flight_id}"
+dropsonde_dir = f"/Volumes/ORCESTRA/HALO-{flight_id}a/dropsondes"
 
-# Flight dataset
-
+# Grid flight dataset from Level 1
 dropsonde_ds = grid_together(dropsonde_dir)
 dropsonde_ds = derived_products(dropsonde_ds)
 
