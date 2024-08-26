3adbc3191d005d4bdea2dfd4fc5741f68f165f37
hgloeckner <helene.gloeckner@posteo.de>
2024-08-26 14:11:29 +0200

    add rough segments until 20240822

--- droputils/rough_segments.py
+++ droputils/rough_segments.py
@@ -2,9 +2,19 @@ starts = {
     "20240811": dict(south="14:10", center="16:00", north="17:18", atr="19:02"),
     "20240813": dict(south="17:30", center="19:00", north="21:00", atr="22:20"),
     "20240816": dict(south="13:50", center="15:05", north="16:50", atr="19:04"),
+    "20240818": dict(south="14:15", center="12:50", north="11:20"),
+    "20240821": dict(south="14:10", north="16:50", extra="17:52"),
+    "20240822": dict(
+        south="12:50", center="13:55", north="16:30", atr1="17:08", atr2="18:48"
+    ),
 }
 ends = {
     "20240811": dict(south="15:20", center="17:15", north="18:23", atr="19:59"),
     "20240813": dict(south="18:50", center="20:15", north="22:15", atr="23:05"),
     "20240816": dict(south="14:54", center="16:03", north="17:50", atr="19:50"),
+    "20240818": dict(south="15:30", center="14:00", north="12:25"),
+    "20240821": dict(south="15:07", north="17:50", extra="18:55"),
+    "20240822": dict(
+        south="13:49", center="14:56", north="17:01", atr1="18:05", atr2="19:20"
+    ),
 }
