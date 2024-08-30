dd813eea6ca7e68e4c04c7aab0118f9af2beba3b
Nina Robbins Blanch <nina.robbins@mpimet.mpg.de>
2024-08-30 10:19:30 -0100

    flight on 2024-08-29

--- plotting/quicklook_scripts/quicklooks_HALO-20240829a.py
+++ plotting/quicklook_scripts/quicklooks_HALO-20240829a.py
@@ -8,12 +8,12 @@ sys.path.append(os.path.abspath(os.path.join(os.getcwd(), "../")))
 from plotting_tools import all_quicklook_plots
 from datetime import datetime
 
-flight_time = datetime(2024, 8, 22, 0, 0, 0)
+flight_time = datetime(2024, 8, 29, 0, 0, 0)
 flight_id = flight_time.strftime("%Y%m%d")
 
 # Flight directory
-# dropsonde_dir = f"/Volumes/ORCESTRA/HALO-{flight_id}a/dropsondes"
 dropsonde_dir = f"/Users/ninarobbins/Desktop/PhD/ORCESTRA/HALO-DATA/{flight_id}"
+# dropsonde_dir = f"/Volumes/ORCESTRA/HALO-{flight_id}a/dropsondes"
 
 # Grid flight dataset from Level 1
 dropsonde_ds = grid_together(dropsonde_dir)
