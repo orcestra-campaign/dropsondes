298177c9cf7813db00312882325e9e832ae641de
hgloeckner <helene.gloeckner@posteo.de>
2024-09-21 00:03:39 +0200

    add dropsonde products

--- ipfs/hashes_products.py
+++ ipfs/hashes_products.py
@@ -2,6 +2,8 @@
 import subprocess
 import ruamel.yaml
 import sys
+import os
+from tqdm import tqdm
 
 sys.path.append("./")
 sys.path.append("../")
@@ -35,17 +37,33 @@ config = data_utils.get_config(config_path)
 product_path = config["OPTIONAL"]["product_dir"]
 # %%
 
-# add raw data to tree
-
 # Ensure the structure exists and initialize empty dicts if they don't exist
 tree["products"]["HALO"].setdefault("dropsondes", {})
-
-
 if tree["products"]["HALO"]["dropsondes"] is None:
     tree["products"]["HALO"]["dropsondes"] = {}
-# add the hashes to the tree
-product_sonde = path2ipfs(product_path)
-tree["products"]["HALO"]["dropsondes"] = product_sonde
+
+
+# add product_data
+level_dirs = ["Level_1", "Level_2"]
+for level in tqdm(level_dirs):
+    print(level)
+    tree["products"]["HALO"]["dropsondes"].setdefault(f"{level}", {})
+    if tree["products"]["HALO"]["dropsondes"][f"{level}"] is None:
+        tree["products"]["HALO"]["dropsondes"][f"{level}"] = {}
+    fl_path = os.path.join(product_path, level)
+    flight_ids = [f for f in sorted(os.listdir(fl_path)) if "HALO-" in f]
+    for flight in flight_ids:
+        path_to_flight = os.path.join(fl_path, flight)
+        print(path_to_flight)
+        lev_sonde = path2ipfs(path_to_flight)
+
+        tree["products"]["HALO"]["dropsondes"][f"{level}"][f"{flight}"] = lev_sonde
+
+level_dirs = ["Level_3", "Level_4"]
+for level in tqdm(level_dirs):
+    path2lev = os.path.join(product_path, level)
+    lev_sonde = path2ipfs(path2lev)
+    tree["products"]["HALO"]["dropsondes"][f"{level}"] = lev_sonde
 
 # Save the updated tree back to the YAML file
 with open("../../ipfs_tools/tree.yaml", "w") as file:
