177620a9ddb1d34a9e52c211efa7255a6e92853b
Theresa Mieslinger <theresa.mieslinger@gmail.com>
2025-03-25 16:21:57 +0100

    add PDFS for IWV and near-sfc wind speed

--- plots/set_atr_into_context.py
+++ plots/set_atr_into_context.py
@@ -25,6 +25,8 @@ atr_circle_segments = [
 l3 = xr.open_dataset(
     f"{settings.root}/products/HALO/dropsondes/Level_3/PERCUSION_Level_3.zarr",
     engine="zarr",
+).assign(
+    wspd_sfc=lambda ds: ds.w_spd.sel(altitude=slice(0, 51)).mean("altitude")
 )
 l4 = xr.open_dataset(
     f"{settings.root}/products/HALO/dropsondes/Level_4/PERCUSION_Level_4.zarr",
@@ -125,8 +127,54 @@ for ax, var in zip(axes, ["div", "omega"]):
     ax.set_ylabel("Altitude / m" if ax == axes[0] else "")
     ax.set_ylim(0, 15000)
     ax.axvline(0, color="k", linewidth=0.5, zorder=0)
+    ax.set_title("")
 axes[1].legend(loc="upper right", bbox_to_anchor=(1.03, 1.03))
 axes[0].set_xlabel("Divergence / s-1")
 axes[1].set_xlabel("Vertical velocity / Pa s-1")
 
 fig.savefig("../images/profiles_east_atr_div_omega.png", dpi=300, bbox_inches="tight")
+
+# %% Plot the IWV histograms and WS histograms, again for PERCUSION, East, ATR
+fig, axes = plt.subplots(1, 2, figsize=(14, 7))
+for ds, region in zip([l3, l3_east, l3_atr], ["all", "east", "atr"]):
+    for ax, var, bins in zip(axes, ["iwv", "wspd_sfc"], [np.arange(28, 74, 2), np.arange(0, 30, 1)]):
+        ds[var].plot.hist(ax=ax, bins=bins,
+                          density=True, histtype='step', linewidth=3,
+                          color=colors[region], label=labels[region])
+        ax.axvline(ds[var].median().values, color=colors[region], linewidth=1)
+        ax.set_title("")
+
+ax.legend(loc="upper right")
+fig.savefig("../images/hist_east_atr_iwv_ws.png", dpi=300, bbox_inches="tight")
+
+# %% Plot PDFs instead of histograms
+def density(x, mu, sigma=0.2):
+    return (1 / (np.sqrt(2 * np.pi * sigma**2)) * np.exp(-0.5 * ((x - mu) / sigma)**2))
+
+# %%
+fig, ax = plt.subplots(figsize=(12, 7))
+for ds, region in zip([l3, l3_west, l3_east_no_atr, l3_atr], ["all", "west", "east", "atr"]):
+    density(x=xr.DataArray(data=np.arange(0, 80, 1), dims="nodes"),
+            mu=ds.iwv,
+            sigma=2,
+            ).mean("sonde").plot(x="nodes", color=colors[region], label=labels[region])
+ax.set_xlim(25, 75)
+ax.legend(loc="upper left")
+ax.set_ylabel("Probability Density")
+ax.set_xlabel("Integrated water vapor / kg m-2")
+ax.set_title("")
+fig.savefig("../images/pdf_east_atr_iwv.png", dpi=300, bbox_inches="tight")
+
+# %%
+fig, ax = plt.subplots(figsize=(12, 7))
+for ds, region in zip([l3, l3_west, l3_east_no_atr, l3_atr], ["all", "west", "east", "atr"]):
+    density(x=xr.DataArray(data=np.arange(0, 40, 1), dims="nodes"),
+            mu=ds.wspd_sfc,
+            sigma=1,
+            ).mean("sonde").plot(x="nodes", color=colors[region], label=labels[region])
+ax.set_xlim(0, 35)
+ax.legend(loc="upper right")
+ax.set_ylabel("Probability Density")
+ax.set_xlabel("Near-surface wind speed / m s-1")
+ax.set_title("")
+fig.savefig("../images/pdf_east_atr_nsfc_ws.png", dpi=300, bbox_inches="tight")
