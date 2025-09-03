e32d9502e4e64725a121e8d5c716ceeea7a09700
hgloeckner <helene.gloeckner@posteo.de>
2025-09-03 13:54:55 +0200

    alternative to sonde relevance plot - relative

--- plots/omega_sonde_relevance.py
+++ plots/omega_sonde_relevance.py
@@ -7,6 +7,7 @@ import seaborn as sns
 import cmocean as cmo  # noqa
 from xhistogram.xarray import histogram
 import settings
+from droputils import data_utils as du
 
 
 # %%
@@ -14,15 +15,44 @@ lev4 = xr.open_dataset(
     f"{settings.root}/products/HALO/dropsondes/Level_4/PERCUSION_Level_4.zarr",
     engine="zarr",
 )
+# %%
+lev4 = xr.open_dataset(
+    "~/Documents/Data/Dropsonde/dropsonde_data/products/Level_4/PERCUSION_Level_4.zarr",
+    engine="zarr",
+)
+# %%
+
+spc = du.get_circle_id_for_sondes(lev4)
+spc = spc.assign(
+    omega_sonde=(
+        ("sonde", "altitude"),
+        np.concat(
+            [
+                np.stack(
+                    ([spc.sel(circle=circle).omega.values],)
+                    * int(spc.sel(circle=circle).sondes_per_circle.values),
+                    axis=0,
+                ).squeeze()
+                for circle in spc.circle
+            ],
+            axis=0,
+        ),
+    ),
+)
 
 # %%
+plt_var = "rel_error"  # "omega_sonde_relevance"
 cm = 1 / 2.54
+
 plt.style.use("./beach.mplstyle")
 sns.set_palette("bright")
 err = lev4.omega_sonde_relevance
-binsize = 200
+binsize = 50
 sig_om_err = err  # .where(original_values > 1)
-bins_div = np.linspace(sig_om_err.min().values, sig_om_err.max().values, binsize)
+sig_om_err = (err / spc.omega_sonde.where(spc.omega_sonde != 0)).rename("rel_error")
+bins_div = np.linspace(
+    -2, 2, binsize
+)  # np.linspace(sig_om_err.min().values, sig_om_err.max().values, binsize)
 fig, axes = plt.subplots(
     nrows=2,
     ncols=3,
@@ -36,22 +66,47 @@ hist = histogram(sig_om_err, sig_om_err.altitude, bins=[bins_div, binsize])
 im = hist.where(hist != 0).plot(
     cmap="cmo.ice_r",
     vmin=0,
-    vmax=100,
+    # vmax=100,
     ax=axes[1, 0],
     add_colorbar=False,
     rasterized=True,
     y="altitude_bin",
 )
+for ax in axes[:, 0]:
+    ax.axvline(sig_om_err.quantile(0.1), color="k")
+    ax.axvline(sig_om_err.quantile(0.9), color="k")
+axes[1, 0].annotate(
+    "10th percentile",
+    xy=(-0.75, 13500),
+    xytext=(-0.75, 13500),
+    fontsize=3,
+    rotation=270,
+    ha="right",
+    va="top",
+)
+axes[1, 0].annotate(
+    "90th percentile",
+    xy=(0.75, 13500),
+    xytext=(0.75, 13500),
+    fontsize=3,
+    rotation=270,
+    ha="left",
+    va="top",
+)
+
+# axes[1,0].axvline(-1, color="red")
+# axes[1,0].axvline(1, color="red")
 
 axes[1, 0].set_ylabel("altitude / m")
-axes[1, 0].set_xlabel("difference in omega \n if sonde is removed / hPa hr-1")
+axes[1, 0].set_xlabel("relative difference in omega \n if sonde is removed ")
 
 sns.histplot(
-    sig_om_err.to_dataframe()["omega_sonde_relevance"]
+    sig_om_err.to_dataframe()[plt_var]
     .reset_index()
     .drop("altitude", axis=1)
     .drop("sonde", axis=1),
     bins=200,
+    binrange=(-2, 2),
     stat="probability",
     alpha=0.5,
     color="#00267f",
@@ -69,7 +124,7 @@ cbar = fig.colorbar(
     cax=cbaxes,
     orientation="vertical",
     fraction=1,
-    extend="max",
+    # extend="max",
 )
 cbar.ax.tick_params(pad=0.2, length=2)
 cbar.set_label("count", size=4, labelpad=-1)
