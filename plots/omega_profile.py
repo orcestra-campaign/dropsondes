b480aaf7ec09fe140e046345190f09932c4e5583
hgloeckner <helene.gloeckner@posteo.de>
2025-09-30 19:14:33 +0200

    adapt sonde relevance plot

--- plots/omega_profile.py
+++ plots/omega_profile.py
@@ -1,11 +1,11 @@
 # %%
 import xarray as xr
-import numpy as np
 import os
 import matplotlib.pyplot as plt
 import seaborn as sns
 import eurec4a
 import settings
+import droputils.data_utils as du
 
 # %%
 l4_path = f"{settings.root}/products/HALO/dropsondes/Level_4/"
@@ -20,27 +20,34 @@ def assign_island(lon):
         return "BB"
 
 
-island = lev4.aircraft_longitude.to_series().apply(assign_island)
-circle_island = lev4.circle_lon.to_series().apply(assign_island)
-ds_island = xr.DataArray(island, dims="sonde", name="island")
-c_island = xr.DataArray(circle_island, dims="circle", name="c_island")
-ds = lev4.assign_coords(island=ds_island, c_island=c_island)
-
-# %% special ticks
-
-
-ta_sal = ds.where(ds.aircraft_longitude > -40, drop=True).sel(circle=0).ta.mean("sonde")
-ta_bb = (
-    ds.where(ds.aircraft_longitude < -40, drop=True).isel(circle=-1).ta.mean("sonde")
+east = du.sel_sub_domain(
+    lev4,
+    settings.east_region,
+    item_var="circle",
+    lon_var="circle_lon",
+    lat_var="circle_lat",
+)
+west = du.sel_sub_domain(
+    lev4,
+    settings.west_region,
+    item_var="circle",
+    lon_var="circle_lon",
+    lat_var="circle_lat",
+)
+north = du.sel_sub_domain(
+    lev4,
+    settings.north_region,
+    item_var="circle",
+    lon_var="circle_lon",
+    lat_var="circle_lat",
 )
 
-sal_freeze = np.abs(ta_sal - 273.15).argmin()
-bb_freeze = np.abs(ta_bb - 273.15).argmin()
 
 # %% omega vs joanne
 
 csal = settings.colors["csal_mean"]
 cbb = settings.colors["cbb_mean"]
+cnorth = settings.colors["atr_mean"]
 
 cm = 1 / 2.54
 cat = eurec4a.get_intake_catalog()
@@ -48,44 +55,52 @@ joanne = cat.dropsondes.JOANNE.level4.to_dask()
 plt.style.use("./beach.mplstyle")
 fig, axes = plt.subplots(ncols=2, figsize=(12 * cm, 6 * cm))
 
-ds.omega.mean("circle").plot(y="altitude", label="BEACH", color="k", ax=axes[1])
-
-ds.omega.where(ds.omega.c_island == "SAL").mean("circle").plot(
-    y="altitude", label="BEACH East Atlantic", color=csal, ax=axes[1]
+(lev4.omega.mean("circle") * 0.01 * 60 * 60).plot(
+    y="altitude", label="BEACH", color="k", ax=axes[1]
 )
-ds.omega.where(ds.omega.c_island == "BB").mean("circle").plot(
-    y="altitude", label="BEACH West Atlantic", color=cbb, ax=axes[1]
+
+(east.omega.mean("circle") * 0.01 * 60 * 60).plot(
+    y="altitude", label="BEACH East", color=csal, ax=axes[1]
+)
+(west.omega.mean("circle") * 0.01 * 60 * 60).plot(
+    y="altitude", label="BEACH West", color=cbb, ax=axes[1]
+)
+(north.omega.mean("circle") * 0.01 * 60 * 60).sel(altitude=slice(0, 10000)).plot(
+    y="altitude", label="BEACH North", color=cnorth, ax=axes[1]
 )
 
 (joanne.omega * 60 * 60 / 100).sel(alt=slice(0, 9500)).mean("circle").plot(
     ax=axes[1], y="alt", color="C1", label="JOANNE"
 )
-ds.div.mean("circle").plot(y="altitude", label="BEACH ", color="k", ax=axes[0])
 
-ds.div.where(ds.omega.c_island == "SAL").mean("circle").plot(
-    y="altitude", label="BEACH East Atlantic", color=csal, ax=axes[0]
-)
-ds.div.where(ds.omega.c_island == "BB").mean("circle").plot(
-    y="altitude", label="BEACH West Atlantic", color=cbb, ax=axes[0]
+lev4.div.mean("circle").plot(y="altitude", label="BEACH", color="k", ax=axes[0])
+east.div.mean("circle").plot(y="altitude", label="BEACH East", color=csal, ax=axes[0])
+west.div.mean("circle").plot(y="altitude", label="BEACH West", color=cbb, ax=axes[0])
+north.div.mean("circle").sel(altitude=slice(0, 10000)).plot(
+    y="altitude", label="BEACH North", color=cnorth, ax=axes[0]
 )
 joanne.D.mean("circle").plot(ax=axes[0], y="alt", color="C1", label="JOANNE")
 
-axes[1].set_xlabel(f"omega / {ds.omega.attrs['units']}")
-axes[0].set_xlabel(f"divergence / {ds.div.attrs['units']}")
+axes[1].set_xlabel("omega / hPa hr-1")
+axes[0].set_xlabel(f"divergence / {lev4.div.attrs['units']}")
 axes[1].set_ylabel("")
 axes[1].set_yticklabels("")
-axes[0].set_ylabel(f"altitude / {ds.altitude.attrs['units']}")
+axes[0].set_ylabel(f"altitude / {lev4.altitude.attrs['units']}")
 axes[0].legend(loc="center right")
-freeze_alt = (sal_freeze + bb_freeze) / 2 * 10
 axes[0].set_yticks(
-    list(axes[0].get_yticks()) + [freeze_alt],
-    labels=list(axes[0].get_yticks()) + ["273.15 K"],
+    list(axes[0].get_yticks()),
+    labels=list(axes[0].get_yticks()),
 )
 
 for ax in axes:
     ax.set_ylim(0, 13500)
     ax.axvline(0, color="gray", alpha=0.5)
-    ax.axhline(freeze_alt, color="gray", alpha=0.5, linestyle=":")
 
 sns.despine(offset={"left": 5})
 fig.savefig("../images/joanne_vs_beach.pdf", bbox_inches="tight")
+# %%
+
+fig, ax = plt.subplots()
+
+ax.scatter(lev4.circle_lon, lev4.circle_lat, color="red")
+ax.scatter(east.circle_lon, east.circle_lat, color="red")
--- plots/omega_sonde_relevance.py
+++ plots/omega_sonde_relevance.py
@@ -7,6 +7,7 @@ import seaborn as sns
 import cmocean as cmo  # noqa
 from xhistogram.xarray import histogram
 import settings
+from droputils import data_utils as du
 
 
 # %%
@@ -16,13 +17,39 @@ lev4 = xr.open_dataset(
 )
 
 # %%
+
+spc = du.get_circle_id_for_sondes(lev4)
+spc = spc.assign(
+    omega_sonde=(
+        ("sonde", "altitude"),
+        np.concat(
+            [
+                np.stack(
+                    ([spc.sel(circle=circle).omega.values],)
+                    * int(spc.sel(circle=circle).sondes_per_circle.values),
+                    axis=0,
+                ).squeeze()
+                for circle in spc.circle
+            ],
+            axis=0,
+        ),
+    ),
+)
+
+# %%
+plt_var = "omega_sonde_relevance"  # "rel_error"  #
 cm = 1 / 2.54
+
 plt.style.use("./beach.mplstyle")
 sns.set_palette("bright")
-err = lev4.omega_sonde_relevance
-binsize = 200
+err = lev4.omega_sonde_relevance * 0.01 * (60 * 60)
+binsize = 50
 sig_om_err = err  # .where(original_values > 1)
-bins_div = np.linspace(sig_om_err.min().values, sig_om_err.max().values, binsize)
+# sig_om_err = (err / spc.omega_sonde.where(spc.omega_sonde != 0)).rename("rel_error")
+bins_div = np.linspace(
+    sig_om_err.min().values, sig_om_err.max().values, binsize
+)  # -2 to 2
+
 fig, axes = plt.subplots(
     nrows=2,
     ncols=3,
@@ -36,22 +63,50 @@ hist = histogram(sig_om_err, sig_om_err.altitude, bins=[bins_div, binsize])
 im = hist.where(hist != 0).plot(
     cmap="cmo.ice_r",
     vmin=0,
-    vmax=100,
+    # vmax=100,
     ax=axes[1, 0],
     add_colorbar=False,
     rasterized=True,
     y="altitude_bin",
 )
+for ax in axes[:, 0]:
+    (-2 * lev4.omega_sonde_relevance.std("sonde") * 0.01 * (60 * 60)).sel(
+        altitude=slice(0, 13500)
+    ).plot(y="altitude", zorder=10, ax=axes[1, 0], color="k", linewidth=0.5)
+    (2 * lev4.omega_sonde_relevance.std("sonde") * 0.01 * (60 * 60)).sel(
+        altitude=slice(0, 13500)
+    ).plot(y="altitude", zorder=10, ax=axes[1, 0], color="k", linewidth=0.5)
+
+axes[1, 0].annotate(
+    r"-2 $\sigma$",
+    xy=(-3.2, 14500),
+    xytext=(-3.2, 14500),
+    fontsize=5,
+    # rotation=270,
+    ha="right",
+    va="bottom",
+)
+axes[1, 0].annotate(
+    r"2 $\sigma$",
+    xy=(3.2, 14500),
+    xytext=(3.2, 14500),
+    fontsize=5,
+    # rotation=270,
+    ha="left",
+    va="bottom",
+)
+
 
 axes[1, 0].set_ylabel("altitude / m")
-axes[1, 0].set_xlabel("difference in omega \n if sonde is removed / hPa hr-1")
+axes[1, 0].set_xlabel(r"$\Delta \omega$ / hPa hr-1" + "\n if sonde is removed ")
 
 sns.histplot(
-    sig_om_err.to_dataframe()["omega_sonde_relevance"]
+    sig_om_err.to_dataframe()[plt_var]
     .reset_index()
     .drop("altitude", axis=1)
     .drop("sonde", axis=1),
     bins=200,
+    binrange=(-10, 10),
     stat="probability",
     alpha=0.5,
     color="#00267f",
@@ -61,18 +116,27 @@ sns.histplot(
     ax=axes[0, 0],
 )
 axes[0, 0].set_ylabel("")
-sns.despine(offset={"left": 10})
+sns.despine(offset={"left": 5})
 
-cbaxes = axes[1, 0].inset_axes((0.8, 0.03, 0.04, 0.3))
+cbaxes = axes[1, 0].inset_axes((0.73, 0.04, 0.04, 0.3))
 cbar = fig.colorbar(
     im,
     cax=cbaxes,
     orientation="vertical",
     fraction=1,
-    extend="max",
+    # extend="max",
 )
 cbar.ax.tick_params(pad=0.2, length=2)
-cbar.set_label("count", size=4, labelpad=-1)
+cbar.set_label("count", size=5, labelpad=1)
+cbar.ax.tick_params(labelsize=5)
+
+xticks = [-10, 0, 10]
+xticks.append(-2 * sig_om_err.std().values)
+xticks.append(2 * sig_om_err.std().values)
+
+axes[1, 0].set_xticks(
+    xticks, labels=xticks[:-2] + ["{:.2f}".format(x) for x in xticks[-2:]]
+)
 
 # error measure
 sonde_idx = np.insert(np.cumsum(lev4.sondes_per_circle), 0, 0)
@@ -84,27 +148,34 @@ for circle in circles:
         sonde=slice(sonde_idx[circle].values, sonde_idx[circle + 1].values)
     )
     for ax in axes[1, 1:]:
-        ds.omega.plot(y="altitude", ax=ax, label=ds.circle_id.values)
+        omega = ds.omega * 0.01 * (60 * 60)
+        omega_std = ds.omega_std_error * 0.01 * (60 * 60)
+        omega.plot(y="altitude", ax=ax, label=ds.circle_id.values)
     axes[1, 1].fill_betweenx(
         ds.altitude,
-        ds.omega - ds.omega_std_error,
-        ds.omega + ds.omega_std_error,
+        omega - omega_std,
+        omega + omega_std,
         alpha=0.2,
     )
 
     axes[1, 2].fill_betweenx(
         ds.altitude,
-        ds.omega + sonde_ds.omega_sonde_relevance.min(),
-        ds.omega + sonde_ds.omega_sonde_relevance.max(),
+        omega + (sonde_ds.omega_sonde_relevance * 0.01 * (60 * 60)).min(),
+        omega + (sonde_ds.omega_sonde_relevance * 0.01 * (60 * 60)).max(),
         alpha=0.2,
     )
 for ax in axes[1, 1:]:
     ax.set_ylabel("")
-    ax.set_xlabel("omega / hPa hr-1")
-axes[1, 1].set_title("Regression Standard Error")
-axes[1, 2].set_title("Sonde Relevance for Circle")
+    ax.set_xlabel(r"$\omega$ / hPa hr-1")
+axes[1, 1].set_title("Regression Standard Error", fontsize=7)
+axes[1, 2].set_title("Sonde Relevance", fontsize=7)
 axes[1, 1].legend(loc=2, fontsize=3)
 for ax in axes[0, 1:]:
     ax.set_axis_off()
 
 fig.savefig("../images/omega_error_2d.pdf", dpi=300, bbox_inches="tight")
+
+# %%
+(lev4.omega_sonde_relevance.std("sonde") * 0.01 * (60 * 60)).sel(
+    altitude=slice(0, 14000)
+).mean()
