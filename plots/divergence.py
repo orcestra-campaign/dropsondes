17b3cb3252c0ec61318a237b8145bcedb817cdd6
hgloeckner <helene.gloeckner@posteo.de>
2025-09-30 18:36:28 +0200

    adapt divergence

--- plots/divergence.py
+++ plots/divergence.py
@@ -4,6 +4,7 @@ import xarray as xr
 import numpy as np
 import settings
 import eurec4a
+import seaborn as sns
 
 # %%
 l4_path = f"{settings.root}/products/HALO/dropsondes/Level_4/PERCUSION_Level_4.zarr"
@@ -22,6 +23,7 @@ def get_nb_circles_per_flight(ds):
 circle_flights = get_nb_circles_per_flight(ds_lev4)
 # %%
 cm = 1 / 2.54
+sns.set_context("paper", font_scale=0.8)
 plt.style.use("./beach.mplstyle")
 fig, ax = plt.subplots(figsize=(12 * cm, 4 * cm))
 im = (ds_lev4.div).plot(
@@ -34,9 +36,7 @@ im = (ds_lev4.div).plot(
     rasterized=True,
     add_colorbar=False,
 )
-fig.subplots_adjust(right=0.93)
-cax = fig.add_axes([0.95, 0.15, 0.01, 0.7])
-fig.colorbar(im, cax=cax, label="divergence / s-1", extend="both")
+
 nb_circ = 0
 xpos = np.insert(np.cumsum(list(circle_flights.values())) - 0.5, 0, -0.5)
 
@@ -54,51 +54,57 @@ xlabels = (
 )
 
 for x in xpos:
-    ax.axvline(x, color="black")
-ax.set_xticks(xtickpos, labels=xlabels)
-ax.set_xlabel("")
+    ax.axvline(x, color="black", linewidth=0.5)
+ax.set_xticks(xtickpos, labels=xlabels, fontsize=6)
+ax.set_xlabel("date")
 ax.set_ylabel("altitude / m")
+yticks = [0, 2000, 5000, 10000, 15000]
+ax.set_yticks(yticks, labels=yticks, fontsize=6)
 
 ax1 = ax.twiny()
-ax1.set_xticks([xtickpos[11]], labels=["Transfer 0906"])
+ax1.set_xticks([xtickpos[11]], labels=["Transfer 0906"], fontsize=6)
 ax1.set_xlim(ax.get_xlim())
-
+ax1.tick_params(width=0.5)
 ax.annotate(
     "West",
     xy=(230, 110),
     xycoords="figure points",
-    xytext=(250, 118),
-    fontsize=4,
+    xytext=(250, 140),
+    fontsize=7,
     ha="center",
 )
 ax.annotate(
     "",
-    xy=(220, 114),
-    xytext=(280, 114),
+    xy=(220, 136),
+    xytext=(280, 136),
     xycoords="figure points",
     fontsize=6,
-    arrowprops={"arrowstyle": "<->", "linewidth": 0.2},
+    arrowprops={"arrowstyle": "<->", "linewidth": 0.5},
 )
 ax.annotate(
     "East",
-    xy=(120, 110),
+    xy=(120, 140),
     xycoords="figure points",
-    xytext=(100, 118),
-    fontsize=4,
+    xytext=(100, 140),
+    fontsize=7,
     ha="center",
 )
+
 ax.annotate(
     "",
-    xy=(70, 114),
-    xytext=(130, 114),
+    xy=(70, 136),
+    xytext=(130, 136),
     xycoords="figure points",
     fontsize=6,
-    arrowprops={"arrowstyle": "<->", "linewidth": 0.2},
+    arrowprops={"arrowstyle": "<->", "linewidth": 0.5},
 )
 
-# ax.set_ylim(0, 2500)
+fig.subplots_adjust(right=0.93)
+cax = fig.add_axes([0.95, 0.15, 0.01, 0.7])
+cb = fig.colorbar(im, cax=cax, label="divergence / s-1", extend="both")
+cb.set_label(label="divergence / s-1", fontsize=7)
 
-fig.savefig("../images/divergence.pdf", dpi=300, bbox_inches="tight")
+fig.savefig("../images/divergence.pdf", dpi=300, bbox_inches="tight", pad_inches=0.0)
 
 # %%
 # %% div lowest 2500 m
