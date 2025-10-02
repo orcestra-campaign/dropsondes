b22b58d56db9cb58bd1487039cbc0f6b9dec8e21
hgloeckner <helene.gloeckner@posteo.de>
2025-10-02 11:02:38 +0200

    adapt set atr into context

--- plots/set_atr_into_context.py
+++ plots/set_atr_into_context.py
@@ -3,6 +3,7 @@ import numpy as np
 import xarray as xr
 import matplotlib.pyplot as plt
 import settings
+import seaborn as sns
 
 from orcestra import get_flight_segments
 
@@ -25,7 +26,7 @@ atr_circle_segments = [
 l3 = xr.open_dataset(
     f"{settings.root}/products/HALO/dropsondes/Level_3/PERCUSION_Level_3.zarr",
     engine="zarr",
-).assign(wspd_sfc=lambda ds: ds.w_spd.sel(altitude=slice(0, 51)).mean("altitude"))
+).assign(wspd_sfc=lambda ds: ds.wspd.sel(altitude=slice(0, 51)).mean("altitude"))
 l4 = xr.open_dataset(
     f"{settings.root}/products/HALO/dropsondes/Level_4/PERCUSION_Level_4.zarr",
     engine="zarr",
@@ -37,9 +38,9 @@ atr_times = [
     slice(s["start"], s["end"]) for s in segments if "atr_coordination" in s["kinds"]
 ]
 ds_atr_list = [
-    l3.swap_dims({"sonde": "sonde_time"}).sel(sonde_time=t) for t in atr_times
+    l3.swap_dims({"sonde": "launch_time"}).sel(launch_time=t) for t in atr_times
 ]
-l3_atr = xr.concat(ds_atr_list, dim="sonde_time").swap_dims({"sonde_time": "sonde"})
+l3_atr = xr.concat(ds_atr_list, dim="launch_time").swap_dims({"launch_time": "sonde"})
 
 
 # %% Level 4
@@ -55,8 +56,8 @@ l4_atr = (
 
 # Select sondes in East versus West Atlantic
 # %% Level 3
-l3_west = l3.where(l3.aircraft_longitude <= -40, drop=True)
-l3_east = l3.where(l3.aircraft_longitude > -40, drop=True)
+l3_west = l3.where(l3.launch_lon <= -40, drop=True)
+l3_east = l3.where(l3.launch_lon > -40, drop=True)
 sondes_east_no_atr = [
     sid for sid in l3_east.sonde_id.values if sid not in l3_atr.sonde_id.values
 ]
@@ -78,7 +79,6 @@ l4_east_no_atr = (
 )
 
 # %%
-plt.style.use("./beach.mplstyle")
 
 variables = ["theta", "rh", "u", "v"]
 units = ["K", "%", "m s-1", "m s-1"]
@@ -90,16 +90,20 @@ colors = {
     "atr": settings.colors.get("atr", "C2"),
 }
 labels = {
-    "all": "All PERCUSION",
-    "west": "West Atlantic",
-    "east": "East Atlantic\nwithout ATR",
-    "atr": "East Atlantic\nonly ATR",
+    "all": "BEACH",
+    "west": "< -40˚E",
+    "east": "> -40˚E\nwithout ATR",
+    "atr": "> -40˚E\nonly ATR",
 }
 
 altmax = 11200
 
 # %% Profiles of theta, rh, u, v
-fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(14, 16), sharey=True)
+cm = 1 / 2.54
+sns.set_context("paper", font_scale=0.8)
+plt.style.use("./beach.mplstyle")
+
+fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(14 * cm, 16 * cm), sharey=True)
 print(axes.flatten())
 for ax, var, unit in zip(axes.flatten(), variables, units):
     for ds, region in zip(
@@ -114,7 +118,7 @@ for ax, var, unit in zip(axes.flatten(), variables, units):
         l4_atr.sel(circle=circle)[var + "_mean"].plot(
             ax=ax, y="altitude", c=colors["atr"], alpha=0.1, zorder=1
         )
-    ax.set_ylabel("Altitude / m" if ax == axes[0, 0] or ax == axes[1, 0] else "")
+    ax.set_ylabel("altitude / m" if ax == axes[0, 0] or ax == axes[1, 0] else "")
     ax.set_xlabel(f"{var} / {unit}")
     # ax.fill_between([l3[var].min(), l3[var].max()], alt_lim, 15000, color="white", alpha=.7, zorder=10)
     ax.set_ylim(0, 15000)
@@ -123,33 +127,48 @@ axes[0, 0].legend()
 axes[1, 0].set_xlim(-20, 10)
 axes[1, 1].set_xlim(-10, 15)
 
-fig.savefig("../images/profiles_east_atr_q_theta_u_v.png", dpi=300, bbox_inches="tight")
+fig.savefig("../images/profiles_east_atr_q_theta_u_v.pdf", dpi=300, bbox_inches="tight")
 
 # %% Profiles of divergence and omega
-fig, axes = plt.subplots(ncols=2, figsize=(14, 7), sharey=True)
-for ax, var in zip(axes, ["div", "omega"]):
-    for ds, region in zip(
-        [l4, l4_west, l4_east_no_atr, l4_atr], ["all", "west", "east", "atr"]
-    ):
-        ds[var].mean("circle").sel(altitude=slice(0, altmax)).plot(
-            ax=ax, y="altitude", c=colors[region], label=labels[region]
-        )
-    for circle in l4_atr.circle:
-        l4_atr.sel(circle=circle)[var].plot(
-            ax=ax, y="altitude", c=colors["atr"], alpha=0.1, zorder=1
-        )
-    ax.set_ylabel("Altitude / m" if ax == axes[0] else "")
-    ax.set_ylim(0, 15000)
-    ax.axvline(0, color="k", linewidth=0.5, zorder=0)
-    ax.set_title("")
-axes[1].legend(loc="upper right", bbox_to_anchor=(1.03, 1.03))
-axes[0].set_xlabel("Divergence / s-1")
-axes[1].set_xlabel("Vertical velocity / Pa s-1")
 
-fig.savefig("../images/profiles_east_atr_div_omega.png", dpi=300, bbox_inches="tight")
+
+cm = 1 / 2.54
+sns.set_context("paper", font_scale=0.8)
+plt.style.use("./beach.mplstyle")
+fig, axes = plt.subplots(ncols=2, figsize=(12 * cm, 6 * cm), sharey=True)
+# for ax, var in zip(axes, ["div", "omega"]):
+for ds, region in zip(
+    [l4, l4_west, l4_east_no_atr, l4_atr], ["all", "west", "east", "atr"]
+):
+    ds["div"].mean("circle").sel(altitude=slice(0, altmax)).plot(
+        ax=axes[0], y="altitude", c=colors[region], label=labels[region]
+    )
+    (ds["omega"] * 0.01 * 60 * 60).mean("circle").sel(altitude=slice(0, altmax)).plot(
+        ax=axes[1], y="altitude", c=colors[region], label=labels[region]
+    )
+for circle in l4_atr.circle:
+    (l4_atr.sel(circle=circle)["omega"] * 0.01 * 60 * 60).plot(
+        ax=axes[1], y="altitude", c=colors["atr"], alpha=0.1, zorder=1
+    )
+    (l4_atr.sel(circle=circle)["div"]).plot(
+        ax=axes[0], y="altitude", c=colors["atr"], alpha=0.1, zorder=1
+    )
+
+
+for ax in axes:
+    ax.set_title("")
+    ax.axvline(0, color="k", linewidth=0.5, zorder=0)
+axes[1].legend(loc="upper right", bbox_to_anchor=(1.03, 1.03))
+axes[0].set_xlabel("divergence / s-1")
+axes[1].set_xlabel("$\omega$ / hPa hr-1")
+axes[0].set_ylabel("altitude / m")
+axes[0].set_ylim(0, 15000)
+axes[1].set_ylabel("")
+
+fig.savefig("../images/profiles_east_atr_div_omega.pdf", dpi=300, bbox_inches="tight")
 
 # %% Plot the IWV histograms and WS histograms, again for PERCUSION, East, ATR
-fig, axes = plt.subplots(1, 2, figsize=(14, 7))
+fig, axes = plt.subplots(1, 2, figsize=(14 * cm, 7 * cm))
 for ds, region in zip([l3, l3_east, l3_atr], ["all", "east", "atr"]):
     for ax, var, bins in zip(
         axes, ["iwv", "wspd_sfc"], [np.arange(28, 74, 2), np.arange(0, 30, 1)]
@@ -167,7 +186,7 @@ for ds, region in zip([l3, l3_east, l3_atr], ["all", "east", "atr"]):
         ax.set_title("")
 
 ax.legend(loc="upper right")
-fig.savefig("../images/hist_east_atr_iwv_ws.png", dpi=300, bbox_inches="tight")
+fig.savefig("../images/hist_east_atr_iwv_ws.pdf", dpi=300, bbox_inches="tight")
 
 
 # %% Plot PDFs instead of histograms
@@ -176,7 +195,7 @@ def density(x, mu, sigma=0.2):
 
 
 # %%
-fig, ax = plt.subplots(figsize=(12, 7))
+fig, ax = plt.subplots(figsize=(12 * cm, 7 * cm))
 for ds, region in zip(
     [l3, l3_west, l3_east_no_atr, l3_atr], ["all", "west", "east", "atr"]
 ):
@@ -187,13 +206,13 @@ for ds, region in zip(
     ).mean("sonde").plot(x="nodes", color=colors[region], label=labels[region])
 ax.set_xlim(25, 75)
 ax.legend(loc="upper left")
-ax.set_ylabel("Probability Density")
-ax.set_xlabel("Integrated water vapor / kg m-2")
+ax.set_ylabel("probability Density")
+ax.set_xlabel("integrated water vapor / kg m-2")
 ax.set_title("")
-fig.savefig("../images/pdf_east_atr_iwv.png", dpi=300, bbox_inches="tight")
+fig.savefig("../images/pdf_east_atr_iwv.pdf", dpi=300, bbox_inches="tight")
 
 # %%
-fig, ax = plt.subplots(figsize=(12, 7))
+fig, ax = plt.subplots(figsize=(12 * cm, 7 * cm))
 for ds, region in zip(
     [l3, l3_west, l3_east_no_atr, l3_atr], ["all", "west", "east", "atr"]
 ):
