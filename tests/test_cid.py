3b0289ee24661b94c0e73cf1bb0e307008365d9d
hgloeckner <helene.gloeckner@posteo.de>
2026-04-21 16:17:05 +0200

    adapt cid test

--- tests/test_cid.py
+++ tests/test_cid.py
@@ -2,15 +2,22 @@ import plots.settings as settings
 import xarray as xr
 
 
+def test_open_l2():
+    xr.open_dataset(
+        f"ipfs://{settings.lev2}",
+        engine="zarr",
+    )
+
+
 def test_open_l3():
     xr.open_dataset(
-        f"{settings.root}/products/HALO/dropsondes/Level_3/PERCUSION_Level_3.zarr",
+        f"ipfs://{settings.lev3}",
         engine="zarr",
     )
 
 
 def test_open_l4():
     xr.open_dataset(
-        f"{settings.root}/products/HALO/dropsondes/Level_4/PERCUSION_Level_4.zarr",
+        f"ipfs://{settings.lev4}",
         engine="zarr",
     )
